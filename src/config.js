07974cdf83aa355947aee2537b1107012e036845
jcfutur <jean.camard@gmail.com>
2020-04-08 15:12:28 +0200

    devurl

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const UrlConfig= {
-    //url: "",
-    url: "http://localhost:5000",
+    url: "https://silly-brattain-13afc5.netlify.com",
+    //url: "http://localhost:5000",
 }
 
 export default UrlConfig;
